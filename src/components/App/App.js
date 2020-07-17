7e15ac8fbb45515f143465ff5180dc38fc8b1b0f
tanchyk <tanchyk.bohdan@gmail.com>
2020-07-17 11:14:56 +0300

    SearchBar can handle the input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,15 +15,16 @@ class App extends React.Component {
       playListTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
     };
 
-    this.updatePlayListName = this.updatePlayListName.bind(this);
-
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+
+    this.updatePlayListName = this.updatePlayListName.bind(this);
+    this.savePlayList = this.savePlayList.bind(this);
+
+    this.search = this.search.bind(this);
   }
 
-  updatePlayListName(name) {
-    this.setState({playListName: name});
-  }
+  // Tracks
 
   addTrack(track) {
     let tracks = this.state.playListTracks;
@@ -44,15 +45,34 @@ class App extends React.Component {
     this.setState({playListTracks: tracks});
   }
 
+  // Playlist
+
+  updatePlayListName(name) {
+    this.setState({playListName: name});
+  }
+
+  savePlayList() {
+    let trackURIs = this.state.playListTracks.map(track => track.uri);
+
+  }
+
+  // Search
+
+  search(term) {
+    console.log(term);
+  }
+
+  // Render
+
   render() {
     return (
       <div>
         <h1>my<span className="highlight">mix</span></h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} onRemove={this.removeTrack} />
-            <PlayList playListName={this.state.playListName} playListTracks={this.state.playListTracks} onNameChange={this.updatePlayListName}/>
+            <PlayList playListName={this.state.playListName} playListTracks={this.state.playListTracks} onNameChange={this.updatePlayListName} onSave={this.savePlayList}/>
           </div>
         </div>
       </div>
